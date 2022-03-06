8a949256a22699c50e258a668d4246dfd1cd8bb8
jgoriginal <932274204@qq.com>
2022-03-06 14:24:03 +0800

    完成CategorySection的UI

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -8,7 +8,6 @@ const TagsSection = styled.section`
   padding: 12px 16px;
   ol {margin: 0 -12px;
     li {
-      
       display: inline-block;color: #d3d4d8;
       background: #36363f;font-size: 14px;margin: 8px 12px;
       padding: 3px 18px;border-radius: 18px;cursor: pointer;
@@ -21,10 +20,47 @@ const TagsSection = styled.section`
     padding: 2px 4px;border-bottom: 1px solid #6d6e73;}
 `
 const NotesSection = styled.section`
-
+  border-bottom: 1px solid #26272b;
+  label {
+    display: flex;padding: 0 16px;align-items: center;
+    color:#d3d4d8;background:  #17181c ;
+    span {display: flex;align-items: center;white-space: nowrap;
+      margin-right:12px ;
+      >svg {width: 1.3em;margin-right:4px ;
+      }
+    }
+    input {display: block;width: 100%;height: 72px;
+      background: none;border: none;color:#d3d4d8;
+    }
+  }
 `
 const CategorySection = styled.section`
+  background: #17181c ;
+  border-bottom: 1px solid #26272b;
+ >ol {
+   display: flex;
 
+   > li {
+     width: 50%;
+     text-align: center;
+     font-size: 24px;
+     padding: 16px 0;
+     color: #d3d4d8;
+     &.selected {
+       position: relative;  
+       &::after{
+         content: '';
+         position:absolute;
+         display: block;
+         width: 100%;
+         background: #d3d4d8;
+         height: 3px;
+         left:0;
+         bottom: 0;
+       } 
+     }
+   }
+ }
 `
 const NumberPadSection = styled.section `
 
@@ -41,27 +77,18 @@ function Money() {
           <li>衣</li>
           <li>食奥术大师多多</li>
           <li>住</li>
-          <li>行按时发生方法  阿萨德</li>
-          <li>衣</li>
-          <li>食</li>
-          <li>住</li>
-          <li>行</li>
-          <li>衣</li>
-          <li>食</li>
-          <li>住</li>
-          <li>行</li>
         </ol>
         <button><Icon name="add"></Icon>新增标签</button>
       </TagsSection>
       <NotesSection>
         <label>
           <span><Icon name="notes"></Icon>备注</span>
-          <input type="text"/>
+          <input type="text" placeholder={'在这里添加备注'}/>
         </label>
       </NotesSection>
       <CategorySection>
         <ol>
-          <li>支出</li>
+          <li className={"selected"}>支出</li>
           <li>收入</li>
         </ol>
       </CategorySection>
