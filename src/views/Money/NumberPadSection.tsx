de83492190726ad12b7d8068bcb5080cb9aecb13
jgoriginal <932274204@qq.com>
2022-03-07 20:27:09 +0800

    重构NumberPadSection.tsx

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,72 +1,6 @@
-import styled from 'styled-components';
 import React, {useState} from 'react';
-
-const Wrapper = styled.section`
-  display: flex;
-  flex-direction: column;
-
-  .output {
-    background: #17181c;
-    color: #6ec5bd;
-    font-size: 40px;
-    line-height: 72px;
-    display: flex;
-    justify-content: flex-end;
-    padding: 0 16px;
-  }
-
-  .pad {
-    background: #39393b;
-
-    button {
-      transition: all 0.2s;
-      font-size: 18px;
-      color: #e1e1e3;
-      border-radius: 4px;
-      width: 25%;
-      height: 64px;
-      border: 1px solid #1f1f21;
-      background: #39393b;
-      float: left;
-
-      &.ok {
-        float: right;
-        height: 128px;
-        background: linear-gradient(to right, #e1b187, orange);
-        color: white;
-
-        &:active {
-          background: #c5891a;
-        }
-      }
-
-      &.zero {
-        width: 50%;
-      }
-
-      &:first-child, &:nth-child(5), &:nth-child(9), &:nth-child(13) {
-        background: #38383a;
-      }
-
-      &:nth-child(2), &:nth-child(6), &:nth-child(10), &:nth-child(14) {
-        background: #3f3f42;
-      }
-
-      &:nth-child(3), &:nth-child(7), &:nth-child(11) {
-        background: #444448;
-      }
-
-      &:nth-child(4), &:nth-child(8) {
-        background: #444448;
-      }
-    }
-
-    button:active {
-      background: #6f6f72;
-    }
-  }
-`;
-
+import Wrapper from './NumberPadSection/Wrapper';
+import generateOutput from './NumberPadSection/generateOutput';
 
 const NumberPadSection: React.FC = (props) => {
   const [output , _setOutput] = useState<string>('0')   //output设置成了string
@@ -81,46 +15,12 @@ const NumberPadSection: React.FC = (props) => {
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
     if(text === null) return
-    switch (text) {
-      case '0' :
-      case '1' :
-      case '2' :
-      case '3' :
-      case '4' :
-      case '5' :
-      case '6' :
-      case '7' :
-      case '8' :
-      case '9' :
-        if(output === '0'){
-          setOutput(text)
-        }else {
-          setOutput(output + text)
-        }
-        break
-      case '.' :
-        console.log('点')
-        if(output.indexOf('.')>=0){
-          return
-        }else{
-          setOutput(output+text)
-        }
-          break
-      case 'del' :
-        console.log('删除')
-        if(output.length === 1){
-          setOutput('0')
-        }else {
-          setOutput(output.substring(0,output.length - 1))
-        }
-        break
-      case 'C' :
-        console.log('清空')
-        setOutput('0')
-        break
-      case '确定' :
-        console.log('确定')
-        break
+    if (text === '确定') {
+      //ToDO
+      return
+    }
+    if('1234567890.'.split('').concat(['del','C']).indexOf(text) >=0) {
+      setOutput(generateOutput( text , output))
     }
   };
   return (
---
+++ src/views/Money/NumberPadSection/generateOutput.tsx
@@ -0,0 +1,36 @@
+const generateOutput= ( text:string , output="0" )=>{
+switch (text) {
+  case '0' :
+  case '1' :
+  case '2' :
+  case '3' :
+  case '4' :
+  case '5' :
+  case '6' :
+  case '7' :
+  case '8' :
+  case '9' :
+    if(output === '0'){
+      return text
+    }else {
+      return output + text
+    }
+  case '.' :
+    if(output.indexOf('.') >= 0){
+      return output
+    }else{
+     return output + text
+    }
+  case 'del' :
+    if(output.length === 1){
+      return '0'
+    }else {
+      return output.substring(0,output.length - 1) || ''
+    }
+  case 'C' :
+    return '0'
+  default:
+    return ''
+}
+}
+export  default  generateOutput
