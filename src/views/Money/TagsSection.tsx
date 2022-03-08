c7d57830c6477f4e3094e0dd72da330bc8158954
jgoriginal <932274204@qq.com>
2022-03-08 17:21:14 +0800

    创建createId , 让Id自增

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import React from 'react';
 import Icon from '../../components/Icon';
 import useTags from '../../useTags';
+import createId from 'lib/createId';
 
 const Wrapper = styled.section`
   flex-grow: 1;
@@ -47,7 +48,7 @@ const TagsSection:React.FC<Props> = (props)=>{
   const onAddTag = ()=>{
     const tagName = window.prompt('请输入你要添加的标签')
     if (tagName !== null){
-      setTags([...tags , {id:Math.random(),name:tagName }])
+      setTags([...tags , {id:createId(),name:tagName }])
     }else{
       return
     }
