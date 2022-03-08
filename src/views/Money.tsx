ad7f0efb1d38a23f3d9690053356a7b35404d5e7
jgoriginal <932274204@qq.com>
2022-03-08 11:58:26 +0800

    删除多余的代码

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -11,12 +11,6 @@ display:flex;
   flex-direction: column;
   justify-content: flex-end;
 `
-// type Selected ={
-//   tags:string[],
-//   notes:string ,
-//   category:('-' | '+'),
-//   amount:string
-// }
 function Money() {
 const [selected , setSelected] = useState({
   tags :[] as string[],
@@ -32,36 +26,18 @@ const [selected , setSelected] = useState({
   }
   return (
     <MyLayout>
-      {selected.tags.join(',')}
-      {selected.notes}
-      {selected.category}
-      {selected.amount}
       <TagsSection value={selected.tags}
                    onChange={(tags)=>onChange({tags:tags})}
-                   // onChange={ (tags)=>setSelected({
-                   //        ...selected,
-                   //   tags:tags})}
       />
       <NoteSection  value={selected.notes}
                    onChange={(notes)=>onChange( {notes :notes} )}
-                    // onChange={(notes)=>setSelected({
-                    //       ...selected,
-                    //   notes:notes}
                      />
       <CategorySection  value={selected.category}
                         onChange={(category)=>onChange( {category :category} )}
-                        // onChange={(category)=>{setSelected({
-                        //   ...selected,
-                        //   category:category
-                        // }) } }
                           />
       <NumberPadSection  value={selected.amount.toString()}
                          onChange={(amount)=>onChange( {amount :amount} )}
                          onOk={()=>{}}
-                         // onChange={(amount)=>{setSelected({
-                         //   ...selected,
-                         //   amount:amount
-                         // })}}
       />
     </MyLayout>
   )
