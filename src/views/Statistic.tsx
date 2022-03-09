aad1499f90964b87c26250ce2102099eaf4cf74e
jgoriginal <932274204@qq.com>
2022-03-09 22:51:40 +0800

    æ展示分组的记录

--- src/views/Statistic.tsx
+++ src/views/Statistic.tsx
@@ -1,19 +1,24 @@
 import Layout from '../components/Layout';
 import React, {useState} from 'react';
 import {CategorySection} from './Money/CategorySection';
-import useRecords from '../Hooks/useRecords';
+import useRecords, {RecordItem} from '../Hooks/useRecords';
 import styled from 'styled-components';
 import useTags from '../Hooks/useTags';
 import day from 'dayjs';
 
 const Wrapper = styled.div`
   color: #e0e1e5;
+  font-size: 18px;
+  > .date {
+    padding: 10px 16px;
+    background: #36363f ;
+  }
 `;
 const Item = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  font-size: 18px;
+ 
   line-height: 20px;
   padding: 10px 16px;
 
@@ -34,26 +39,47 @@ function Statistic() {
   const [category, setCategory] = useState<('-' | '+')>('-');
   const {records, setRecords} = useRecords();
   const {getName} = useTags();
+  const selectedRecords = records.filter(r => r.category === category )
+ const hash:{ [K:string]:RecordItem[]} = {}
+  selectedRecords.map(r => {
+    const key = day(r.createdAt).format('YYYY年MM月DD日')
+    if(!(key in hash)){
+      hash[key] = []
+    }
+    hash[key].push(r)
+  })
+const array = Object.entries(hash).sort((a,b) => {
+  if(a[0] === b[0]) return 0
+  if(a[0] > b[0]) return -1
+  if(a[0]  < b[0]) return 1
+  return 0
+})
+
   return (
     <Layout>
       <CategorySection value={category} onChange={v => setCategory(v)}/>
 
-      <Wrapper>
-        {records.map(r => {
-          return <Item key={r.amount}>
-            <div className={'tags'}>
-              {r.tagIds.map(i => <span key={i}>{`${getName(i)} `}</span>)}
-            </div>
-            <div className={'notes'}>
-              {r.notes ? r.notes : '无备注'}
-            </div>
-            <div className={'amount'}>
-              { '￥'+parseFloat(r.amount)}
-            </div>
-            {/*时间: {day(r.createdAt).format('YYYY年MM月DD日')}*/}
-          </Item>;
-        })}
-      </Wrapper>
+      {array.map(([date ,record ])=>{
+        return (
+          <Wrapper>
+            <div className={"date"}>{date}</div>
+            {record.map(r => {
+              return <Item key={r.createdAt}>
+                <div className={'tags'}>
+                  {r.tagIds.map(i => <span key={i}>{`${getName(i)} `}</span>)}
+                </div>
+                <div className={'notes'}>
+                  {r.notes ? r.notes : '无备注'}
+                </div>
+                <div className={'amount'}>
+                  { '￥'+parseFloat(r.amount)}
+                </div>
+                {/*时间: {day(r.createdAt).format('YYYY年MM月DD日')}*/}
+              </Item>;
+            })}
+          </Wrapper>
+        )
+      })}
     </Layout>
   );
 }
